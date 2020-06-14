1dd094f4e2d802c6698fad9693aaad98a20ad781
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-06-14 13:01:29 +0530

    Added:-heder and topbar

---
+++ src/components/heder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../CSS/header.css"
+function Header(Prop){
+    return (
+    <div className='header'>
+          <span className='text'>Welcome To Quantity measurement</span>
+        </div>)
+}
+export default Header;
---
+++ src/components/upperBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../CSS/topBar.css"
+function UpperBar(Prop){
+    return (<div className='container'>
+        <div className='quanment'>Quanment</div>
+        <div />
+        <div className='history'>History</div>
+        </div>)
+}
+export default UpperBar;
